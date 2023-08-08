51047b021e0e5ad468118583fe8b2d2ec94b2ebc
claudialioncoADM <claudialionco@gmail.com>
2023-08-08 15:44:27 -0300

    paginação

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -208,10 +208,6 @@
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 @yield('content')
-                <div
-                    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2">Dashboard</h1>
-                </div>
                 
                {{-- <h2>Section title</h2>
                 <div class="table-responsive small">
