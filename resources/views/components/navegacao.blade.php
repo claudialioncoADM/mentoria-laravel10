4a27a19eada094b3f8c085be4fa01b782d7fdbdf
claudialioncoADM <claudialionco@gmail.com>
2023-08-08 13:46:33 -0300

    controller produto

--- resources/views/components/navegacao.blade.php
+++ resources/views/components/navegacao.blade.php
@@ -25,7 +25,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link d-flex align-items-center gap-2" href="#">
+                        <a class="nav-link d-flex align-items-center gap-2" href="{{ route('produto.index') }}">
                             <svg class="bi">
                                 <use xlink:href="#cart" />
                             </svg>
